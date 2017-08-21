d0a8626f6fc5e72e32348d3a54fdbd87f6d0e871
chenyu16197 <chenyu16197@WINDOWS-0HOQBGF.hs.handsome.com.cn>
2017-08-21 16:02:16 +0800

    0

--- ssm/src/main/java/com/company/action/base/TestAction.java
+++ ssm/src/main/java/com/company/action/base/TestAction.java
@@ -4,8 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
 @RequestMapping("/test")
+@Controller
 public class TestAction {
 	@RequestMapping("/test")
 	public String test(ModelMap model){
